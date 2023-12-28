7a36e494bf072ae52ad4856af8c87f1eb7864f31
Chandra Kiran G <chandrakiran.g19@gmail.com>
2023-12-28 11:06:21 +0530

    chore: update workflow file - add DB pool instead of single connection

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 use actix_hello_world::configuration::get_configuration;
 use actix_hello_world::startup::run;
+use sqlx::PgPool;
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Failed to read configuration.");
+
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postres");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await?;
+    run(listener, connection_pool)?.await?;
     Ok(())
 }
