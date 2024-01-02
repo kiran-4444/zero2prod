21c89c871d9de55c420d34b35118b3d583e6abb5
Chandra Kiran G <chandrakiran.g19@gmail.com>
2024-01-02 20:42:19 +0530

    feat: add logging

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use actix_hello_world::configuration::get_configuration;
 use actix_hello_world::startup::run;
+use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     let configuration = get_configuration().expect("Failed to read configuration.");
 
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -10,6 +10,11 @@ pub struct FormData {
 }
 
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    log::info!(
+        "Adding '{}' '{}' as a new subscriber.",
+        form.email,
+        form.name
+    );
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -23,9 +28,12 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
     .execute(pool.get_ref())
     .await
     {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            log::info!("Saved successfully!");
+            HttpResponse::Ok().finish()
+        }
         Err(e) => {
-            println!("Failed to execute query: {}", e);
+            log::error!("Failed to execute query: {:?}", e);
             HttpResponse::InternalServerError().finish()
         }
     }
