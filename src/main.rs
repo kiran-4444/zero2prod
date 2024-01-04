b3f6a92a1b2cda00c8d6f26f3d262388b5d88e2f
Chandra Kiran G <chandrakiran.g19@gmail.com>
2024-01-04 15:16:26 +0530

    feat: add tracing, subscriber for telemetry

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 use actix_hello_world::configuration::get_configuration;
 use actix_hello_world::startup::run;
-use env_logger::Env;
+use actix_hello_world::telemetry::{get_subscriber, init_subscriber};
 use sqlx::PgPool;
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    let subscriber = get_subscriber("zero2prod".into(), "info".into());
+    init_subscriber(subscriber);
+
     let configuration = get_configuration().expect("Failed to read configuration.");
 
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
